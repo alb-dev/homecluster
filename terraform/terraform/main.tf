7331b9b1f4710e11bd133332bb50c1c3aae92ea5
alb-dev <alexander@breidenbach.koeln>
2022-01-22 00:19:47 +0100

    rbased

---
+++ terraform/terraform/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 0.13.0"
+
+  required_providers {
+    proxmox = {
+      source  = "Telmate/proxmox"
+      version = "2.9.4"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_tls_insecure = "true"
+  pm_api_url      = "https://192.168.178.25:8006/api2/json"
+  pm_user         = "root@pam"
+  pm_password     = var.pm_password
+  pm_parallel     = 2
+}
---
+++ terraform/terraform/variable.tf
@@ -0,0 +1,94 @@
+# proxmox hypervisor variables
+variable "pm_password" {}
+# proxmox node variables
+variable "ssh_key" {}
+variable "ciuser" {}
+variable "cipassword" {}
+
+variable "common" {
+  type = map(string)
+  default = {
+    ciuser        = "nao"
+    os_type       = "cloud-init"
+    clone         = "linux-ubuntu-server-20.04"
+    search_domain = "intern.breidenbach.koeln"
+    nameserver    = "192.168.178.1"
+  }
+}
+
+variable "masters" {
+  type = map(map(string))
+  default = {
+    k8s-master01 = {
+      id          = 4010
+      cidr        = "192.168.178.11/24"
+ #     ceph_cidr   = "10.75.33.40/24"
+      cores       = 3
+      gw          = "192.168.178.1"
+      memory      = 4096
+      disk        = "50G"
+      target_node = "pve-01"
+    },
+    k8s-master02 = {
+      id          = 4011
+      cidr        = "192.168.178.12/24"
+#      ceph_cidr   = "10.75.33.41/24"
+      cores       = 3
+      gw          = "192.168.178.1"
+      memory      = 4096
+      disk        = "50G"
+      target_node = "pve-02"
+    },
+    k8s-master03 = {
+      id          = 4012
+      cidr        = "192.168.178.13/24"
+#      ceph_cidr   = "10.75.33.42/24"
+      cores       = 3
+      gw          = "192.168.178.1"
+      memory      = 4096
+      disk        = "50G"
+      target_node = "pve-02"
+    }
+  }
+}
+variable "workers" {
+  type = map(map(string))
+  default = {
+    k8s-worker01 = {
+      id          = 4020
+      cidr        = "192.168.178.14/24"
+      cores       = 4
+      gw          = "192.168.178.1"
+      memory      = 8192
+      disk        = "50G"
+      target_node = "pve-01"
+    },
+    k8s-worker02 = {
+      id          = 4021
+      cidr        = "192.168.178.15/24"
+      cores       = 4
+      gw          = "192.168.178.1"
+      memory      = 8192
+      disk        = "50G"
+      target_node = "pve-01"
+    },
+    k8s-worker03 = {
+      id          = 4022
+      cidr        = "192.168.178.16/24"
+      cores       = 4
+      gw          = "192.168.178.1"
+      memory      = 8192
+      disk        = "50G"
+      target_node = "pve-02"
+    },
+    k8s-worker04 = {
+      id          = 4023
+      cidr        = "192.168.178.233/24"
+      cores       = 4
+      gw          = "192.168.178.1"
+      memory      = 8192
+      disk        = "50G"
+      target_node = "pve-02"
+    },
+  }
+}
