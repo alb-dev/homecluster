8b4846683d3ac169c16deef218930d9139843fb5
alb-dev <alexander@breidenbach.koeln>
2022-05-28 13:29:07 +0200

    cluster rebuild

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -10,7 +10,7 @@ variable "common" {
   default = {
     ciuser        = "alb"
     os_type       = "cloud-init"
-    clone         = "linux-alma-server-8"
+    clone         = "linux-alma-server-9"
     search_domain = "int.swalb.io"
     nameserver    = "192.168.178.5"
   }
@@ -28,24 +28,6 @@ variable "masters" {
       disk0        = "50G"
       target_node = "pve-02"
     }
-    k8s-cp-02 = {
-      id          = 4011
-      cidr        = "192.168.178.13/24"
-      cores       = 4
-      gw          = "192.168.178.1"
-      memory      = 6144
-      disk0        = "50G"
-      target_node = "pve-01"
-    },
-    k8s-cp-03 = {
-      id          = 4012
-      cidr        = "192.168.178.14/24"
-      cores       = 4
-      gw          = "192.168.178.1"
-      memory      = 6144
-      disk0        = "50G"
-      target_node = "pve-01"
-    }
   }
 }
 variable "workers" {
@@ -58,7 +40,7 @@ variable "workers" {
       gw          = "192.168.178.1"
       memory      = 12000
       disk0        = "50G"
-      disk1        = "60G"
+      disk1        = "100G"
       target_node = "pve-02"
     },
     k8s-wk-02 = {
@@ -68,17 +50,17 @@ variable "workers" {
       gw          = "192.168.178.1"
       memory      = 12000
       disk0        = "50G"
-      disk1        = "60G"
+      disk1        = "100G"
       target_node = "pve-02"
     },
     k8s-wk-03 = {
       id          = 4022
       cidr        = "192.168.178.17/24"
-      cores       = 6
+      cores       = 8
       gw          = "192.168.178.1"
-      memory      = 10000
+      memory      = 24000
       disk0        = "50G"
-      disk1        = "60G"
+      disk1        = "250G"
       target_node = "pve-01"
     },
   }
